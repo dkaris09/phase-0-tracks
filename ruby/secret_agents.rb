0f755d89fd86b00d9989c0cde5d08c8a01e13360
Daniel <dakaragiannis@gmail.com>
2016-09-18 20:58:12 -0500

    decrypt 4.6mps

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,13 @@
+def decrypt(string)
+	alphabet = "abcdefghijklmnopqrstuvwxyz"
+	string = gets.chomp
+	length = string.length
+	index = 0
+	result = ""
+
+	until index == length
+		result += alphabet[alphabet.index(string[index])-1]
+		index += 1
+	end
+	puts result
+end
