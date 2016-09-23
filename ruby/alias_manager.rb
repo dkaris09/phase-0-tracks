8bf49610c4cc78b88a6fa46b77730881a91f91e8
Daniel <dakaragiannis@gmail.com>
2016-09-23 17:53:58 -0500

    5.5 and 5.6

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,28 @@
+#name1= "Felicia" 
+#name2= "Torres"
+#swap Felicia with Torres then combine into full name
+#name1
+	#full_name.next("aeiou")
+	#full_name.next("^aeiou")
+
+def spy_method(name1,name2)
+	full_name= name2 + " " + name1
+	full_name.downcase!
+	split_name = full_name.split("")
+	puts split_name
+	split_name.map! do |letter|
+	vowels = "aeiou"
+	index = vowels.index(letter)
+	p index
+	if index == nil
+	letter
+	else
+		vowels[(index+1) % 5]
+	end
+	end
+	p split_name
+end
+spy_method("Felicia","Torres")
+
+vowels = ["a", "e", "i", "o", "u"]
+consonants = ["b","c","d","f","g","h","j","k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
