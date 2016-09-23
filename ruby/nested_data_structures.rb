5b1879a6652e2089268daa79659181691d7baf23
Daniel <dakaragiannis@gmail.com>
2016-09-23 11:37:49 -0500

    5.4 nested structures

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,27 @@
+kennedy_expressway = {
+	red_car: {
+		car_model: "Camry",
+		car_info: {
+			total_seats: 5,
+			seats_taken: 4,
+		},
+		passengers: ["Bill", "Betsy", "Bruce", "Blake"],
+	},
+	blue_car: {
+		car_model: "Focus",
+		car_info: {
+			total_seats: 4,
+			seats_taken: 2,
+		},
+		passengers: ["Chris", "Chloe"],
+	},
+	gray_truck: {
+		truck_model: "F-150",
+		truck_info: {
+			total_seats: 2,
+			seats_taken: 1,
+		},
+		passengers: ["Tim"],
+	}
+}
+
