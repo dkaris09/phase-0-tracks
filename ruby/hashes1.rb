3481fd9a1333ad052d57f14b6172ed31b4d72c00
Daniel <dakaragiannis@gmail.com>
2016-11-01 12:34:39 -0500

    5.2

--- ruby/hashes1.rb
+++ ruby/hashes1.rb
@@ -18,27 +18,50 @@ puts client_info = {
 	phone: phone,
 	decor_theme: decor_theme
 }
-	
+
+# provide user changes for update	
 puts "Make sure we have everything correct. Any changes we should make?"
-changes = gets.chomp 
+changes = gets.chomp.to_sym 
 
 	if changes == "name" 
 		puts "Name:"
 		name = gets.chomp.to_s 
+	
 	elsif changes == "address"
 		puts "Address:"
 		address = gets.chomp
+
 	elsif changes == "children"
 		puts "Do you have children? (yes/no)"
 		children = gets.chomp.to_s
+		
 	elsif changes == "phone"
 		puts "Phone:"
 		phone = gets.chomp.to_i 
+
 	elsif changes == "decor theme"
 		puts "Decor Theme:"
 		decor_theme = gets.chomp.to_s 
+	
 	else 
 		puts "Ok great!"
+	end 
+#convert data entry
+	if changes == :name
+		value = name
+		client_info[changes] = value 
+	elsif changes == :address
+		value = address
+		client_info[changes] = value 
+	elsif changes == :children
+		value = children
+		client_info[changes] = children
+	elsif changes = :phone 
+		value = phone 
+		client_info[changes] = phone 
+	elsif changes == :decor_theme
+		value = decor_theme
+		client_info[changes] = decor_theme
 	end 
 puts "Here is the client info once again with any changes."
 puts client_info
@@ -46,3 +69,4 @@ puts client_info
 #Driver Code 
 client_info[:address] + client_info[:name]
 client_info[:decor_theme]
+puts client_info
