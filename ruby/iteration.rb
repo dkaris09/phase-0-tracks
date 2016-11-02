ce0a4e7c695311f3fc666617355d07b06b2cfd10
Daniel <dakaragiannis@gmail.com>
2016-11-02 13:18:46 -0500

    5.3 MPS

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -40,3 +40,28 @@ buddies.map do |name, location|
 	puts "I know #{name} from #{location}."
 end 
 puts " "
+
+#Release 2: Use the Documentation 
+
+nums = [1,2,3,4,5,6,7,8,9,10]
+buddies = {
+	alexander: "school",
+	george: "church",
+	bob: "school",
+	daniel: "work"
+}
+# deleting any numbers that are less than 5
+ nums.delete_if {|score| score < 5 }
+p nums  	#delete_if
+
+nums = [1,2,3,4,5,6,7,8,9,10]
+# keeping any numbers that are less than 5
+ nums.keep_if {|num| num < 5}
+p nums 	#keep_if
+
+# only items satisfying a certain condition
+puts buddies.select{ |name, location| location == "school"}
+	
+# A method that will remove items from a data structure until the condition in the #block evaluates to false, then stops 
+buddies.reject!{|name, location| location == "school" }
+buddies.reject!{|name, location| location == "school" }
