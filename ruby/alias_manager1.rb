8deab1a51cf69fb2f4ec7f02d27d2355a22b6cef
Daniel <dakaragiannis@gmail.com>
2016-11-04 15:15:03 -0500

    release 2 for 5.6 completed

---
+++ ruby/alias_manager1.rb
@@ -0,0 +1,7 @@
+# Swap the first and last name.
+
+# Changing all of the vowels (a, e, i, o, or u) to the next vowel in 'aeiou'
+
+# swap all of the consonants (everything besides vowels)  
+# to the next consonant in the alphabet.
+
