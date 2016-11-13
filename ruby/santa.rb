c73980e9991da2474bf7a14454f9439cb3419e72
Daniel <dakaragiannis@gmail.com>
2016-11-13 14:46:53 -0600

    release 0 6.3

---
+++ ruby/santa.rb
@@ -0,0 +1,17 @@
+# Release 0: Bring Santa to Life
+class Santa 
+	def initialize
+		puts  "Initializing Santa instance ..."
+	end 
+
+	def speak
+		puts "Ho, ho, ho! Haaaappy holidays!"
+	end 
+
+	def  eat_milk_and_cookies(cookie_type)
+		puts "That was a good #{cookie_type} cookie!" 
+	end 
+end 
+
+# Driver Code
+santa = Santa.new 
